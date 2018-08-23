6d00e312bd4b36fc361e28b1654f467d22711c55
calvinho1214 <calvinho1214@gmail.com>
2018-08-23 13:40:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function myAnimal() {
 }
 
 function yourAnimal() {
+  if(animal === "dog"){
+    animal = "cat";
+  }
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
